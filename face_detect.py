edf1b747bf866c5f9de2005aa6b81bc2f75b46c4
Your Name <you@example.com>
2017-07-15 22:46:45 +0800

    7-15-3

--- face_detect.py
+++ face_detect.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
             cutImg = gray[y:y+h,x:x+w]      #保存灰度人脸图像
             #grayImg = cv2.cvtColor(cutImg, cv2.COLOR_BAYER_BG2GRAY)
             resizeImg = cv2.resize(cutImg, (64,64),interpolation=cv2.INTER_CUBIC)
-            cv2.imwrite(clean_face_path + str(i) + '.jpg', resizeImg)
+            cv2.imwrite(clean_face_path + 'usr_zhang_' + str(i) + '.jpg', resizeImg)
             i += 1
         if cv2.waitKey(10) & 0xFF == ord('q'):   # 当按下"q"键时，将退出循环
             break
