e3dffe973d6d6bc70f807c4e3d862c68d2bf7d4d
colorblank <jie05200@outlook.com>
2017-07-24 12:56:06 +0800

    7-24-2
    新增识别用户切换，摄像头暂停

--- face_detect.py
+++ face_detect.py
@@ -14,7 +14,7 @@ clean_face_path ='/Users/zhang/developer/users_face_file/'
 cascade_fn = '/Users/zhang/miniconda2/pkgs/opencv-3.1.0-np112py27_1/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml'
 cascade = cv2.CascadeClassifier(cascade_fn)
 #eye_cascade = cv2.CascadeClassifier('/Users/zhang/anaconda/pkgs/opencv-2.4.11-py27_1/share/OpenCV/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
-usr_id = 1000
+
 
 def face_detect(img, cascade):
     face = cascade.detectMultiScale(gray, scaleFactor=1.3,minNeighbors=4, minSize=(30, 30)) 
@@ -38,6 +38,7 @@ if __name__ == '__main__':
     point1 = ()
     cap = cv2.VideoCapture(0)
     i = 1
+    usr_id = 1000
     while(True):
         ret, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -56,11 +57,17 @@ if __name__ == '__main__':
         #         break
        
              # 当按下"s"键时，将保存当前画面
+
+            
         for x, y, w, h in rectangle:
             cutImg = gray[y:y+h,x:x+w]      #保存灰度人脸图像
             #grayImg = cv2.cvtColor(cutImg, cv2.COLOR_BAYER_BG2GRAY)
             resizeImg = cv2.resize(cutImg, (100,100),interpolation = cv2.INTER_AREA)
             #保存样本数量为100
+            if cv2.waitKey(25)&0xFF == ord('x'):    #按下‘x’键，切换识别用户
+                usr_id += 1
+            else:
+                pass
             while(i > 100):
                 i = 1
                 cv2.imwrite('{0}{1}{2}{3}{4}'.format(clean_face_path,str(usr_id),'_',str(i),'.jpg'), resizeImg)
@@ -70,9 +77,15 @@ if __name__ == '__main__':
                 cv2.imwrite('{0}{1}{2}{3}{4}'.format(clean_face_path,str(usr_id),'_',str(i),'.jpg'), resizeImg)
                 i += 1
                 break
-        if cv2.waitKey(10) & 0xFF == ord('q'):   # 当按下"q"键时，将退出循环
+            
+        if cv2.waitKey(25)&0xFF == 32:            #按下空格键暂停摄像头
+            cv2.waitKey(0)
+            
+            
+        if cv2.waitKey(25) & 0xFF == ord('q'):   # 当按下'q'键时，将退出循环
             break
+        
 
 cap.release()
-cap.release()
 cv2.destroyAllWindows()
+exit()
